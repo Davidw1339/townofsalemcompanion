89386e10f2d4a98aba1e4214188d4634a2e7cf5d
waxmaster98 <waxman57@hotmail.com>
2016-05-22 17:53:01 -0400

    Finished Ranked Practice functionality

--- js/functions.js
+++ js/functions.js
@@ -34,6 +34,82 @@ var roles = [
 "Witch"
 ];
 
+var Jailor = "Jailor";
+
+var TownInvest = [
+"Investigator",
+"Lookout",
+"Sheriff",
+"Spy"
+];
+
+var TownKilling = [
+"Jailor",
+"Vampire Hunter",
+"Veteran",
+"Vigilante"
+];
+
+var TownProtect = [
+"Bodyguard",
+"Doctor"
+];
+
+var TownSupport = [
+"Escort",
+"Mayor",
+"Medium",
+"Retributionist",
+"Transporter"
+];
+
+var Godfather = "Godfather";
+
+var Mafioso = "Mafioso";
+
+var RandomMafia = [
+"Disguiser",
+"Forger",
+"Framer",
+"Janitor",
+"Blackmailer",
+"Consigliere",
+"Consort"
+];
+
+var NeutralBenign = [
+"Amnesiac",
+"Survivor"
+];
+
+var NeutralEvil = [
+"Executioner",
+"Jester",
+"Witch"
+];
+
+var NeutralKilling = [
+"Arsonist",
+"Serial Killer",
+"Werewolf"
+];
+
+var jailor = false;
+var towninvest = false;
+var towninvest1 = false;
+var townsupport = false;
+var townsupport1 = false;
+var townprotect = false;
+var townkill = false;
+var randomtown = false;
+var godfather = false;
+var mafioso = false;
+var randommaf = false;
+var neutralkill = false;
+var neutralevil = false;
+var neutralbenign = false;
+var any = false;
+
 function getFilteredRoles(text)
 {
 	var matches = [];
@@ -47,6 +123,16 @@ function getFilteredRoles(text)
 	return matches;
 }
 
+function strikeThrough(target){
+    $(".role").each(function(i, obj){
+        text1 = $(this).text();
+        if(text1 == target && $(this).css('textDecoration') != 'line-through'){
+           $(this).css('textDecoration', 'line-through');
+           return false;
+        }
+    });
+}
+
 function updateFilteredList(matches) //makes change to list of roles to click on
 {
 	$("#rolelist").empty();
@@ -54,6 +140,132 @@ function updateFilteredList(matches) //makes change to list of roles to click on
 	{
 		$("#rolelist").append("<span class = 'filteredrole'>" + role + "</span><br>");
 	});
+    
+    $(".filteredrole").click(function(){
+        var text = $(this).text();
+        var text1, target;
+        if(text == Jailor){
+            if(!jailor){
+                target = "Jailor";
+                jailor = true;
+            } else if(!townkill){
+                target = "Town Killing";
+                townkill = true;
+            } else if(!randomtown){
+                target = "Random Town";
+                randomtown = true;
+            } else if(!any){
+                target = "Any";
+                any = true;
+            }
+            strikeThrough(target);
+        } else if($.inArray(text, TownInvest) > -1){
+            if(!towninvest){
+                target = "Town Investigative";
+                towninvest = true;
+            } else if(!towninvest1){
+                target = "Town Investigative";
+                towninvest1 = true;
+            } else if(!randomtown){
+                target = "Random Town";
+                randomtown = true;
+            } else if(!any){
+                target = "Any";
+                any = true;
+            }
+            strikeThrough(target);
+        } else if($.inArray(text, TownSupport) > -1){
+            if(!townsupport){
+                target = "Town Support";
+                townsupport = true;
+            } else if(!townsupport1){
+                target = "Town Support";
+                townsupport1 = true;
+            } else if(!randomtown){
+                target = "Random Town";
+                randomtown = true;
+            } else if(!any){
+                target = "Any";
+                any = true;
+            }
+            strikeThrough(target);
+        } else if($.inArray(text, TownProtect) > -1){
+            if(!townprotect){
+                target = "Town Protective";
+                townprotect = true;
+            } else if(!randomtown){
+                target = "Random Town";
+                randomtown = true;
+            } else if(!any){
+                target = "Any";
+                any = true;
+            }
+            strikeThrough(target);
+        } else if($.inArray(text, TownKilling) > -1){
+            if(!townkill){
+                target = "Town Killing";
+                townkill = true;
+            } else if(!randomtown){
+                target = "Random Town";
+                randomtown = true;
+            } else if(!any){
+                target = "Any";
+                any = true;
+            }
+            strikeThrough(target);
+        } else if(text == "Godfather"){
+            if(!godfather){
+                target = "Godfather";
+                godfather = true;
+            }
+            strikeThrough(target);
+        } else if(text == "Mafioso"){
+            if(!mafioso){
+                target = "Mafioso";
+                mafioso = true;
+            }
+            strikeThrough(target);
+        } else if($.inArray(text, RandomMafia) > -1){
+            if(!randommaf){
+                target = "Random Mafia";
+                randommaf = true;
+            } else if(!any){
+                target = "Any";
+                any = true;
+            }
+            strikeThrough(target);
+        } else if($.inArray(text, NeutralKilling) > -1){
+            if(!neutralkill){
+                target = "Neutral Killing";
+                neutralkill = true;
+            } else if(!any){
+                target = "Any";
+                any = true;
+            }
+            strikeThrough(target);
+        } else if($.inArray(text, NeutralEvil) > -1){
+            if(!neutralevil){
+                target = "Neutral Evil";
+                neutralevil = true;
+            } else if(!any){
+                target = "Any";
+                any = true;
+            }
+            strikeThrough(target);
+        } else if($.inArray(text, NeutralBenign) > -1){
+            if(!neutralbenign){
+                target = "Neutral Benign";
+                neutralbenign = true;
+            } else if(!any){
+                target = "Any";
+                any = true;
+            }
+            strikeThrough(target);
+        }
+        $("#filter").val("");
+        updateFilteredList(roles);
+    });
+    
 }
 
 $(document).ready(function() 
@@ -78,4 +290,5 @@ $(document).ready(function()
 		var roles = getFilteredRoles(text);
 		updateFilteredList(roles);
 	});
+    
 });
