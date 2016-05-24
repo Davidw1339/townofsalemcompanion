58e3389217c46617f2b619e6e43c02b6c9fa469b
waxmaster98 <waxman57@hotmail.com>
2016-05-24 15:08:12 -0400

    Added Privacy Policy and Re-added Contact page

---
+++ contact.php
@@ -0,0 +1,134 @@
+<?php
+    if($_POST["submit"]){
+        if(!$_POST["name"]){
+            $error.="<br />Please enter your name.";
+        }
+        if(!$_POST["email"]){
+            $error.="<br />Please enter your email.";
+        }
+        if(!$_POST["comment"]){
+            $error.="<br />Please enter your comment.";
+        }
+        
+        if ($_POST["email"]!="" AND !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) { 
+            $error.="<br />Please enter a valid email address."; 
+        }
+        if($error){
+            $result="<div class='alert alert-danger'><strong>There were error(s) with your form:</strong> $error</div>";
+        }   
+        else{
+            if(mail("davidw1339@gmail.com", "Comment From Town of Salem Companion", "Name: ".$_POST["name"]."
+            
+            Email: ".$_POST["email"]."
+            
+            Comment: ".$_POST["comment"])){
+                $result="<div class='alert alert-success'><strong>Thank you! Your message was successfully sent.
+                We will be in touch.</strong></div>";
+            } else {
+                $result="<div class='alert alert-danger'><strong>There was an error sending your message.
+                Please try again later.</strong></div>";
+            }
+        }
+        
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+        <title>Town of Salem Companion</title>
+        
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+        <link href="css/custom.css" rel="stylesheet">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+ 
+        <script src="js/bootstrap.min.js"></script>
+
+        <script src="js/functions.js"></script>
+                
+                
+        <style>
+            .center{
+                text-align:center;
+                margin-top:105px;
+            }
+            
+            .emailf{
+                border:1px solid grey;
+                border-radius:10px;
+            }
+            
+            textarea{
+                resize:none;
+                min-height:250px;
+            }
+            
+            .subm{
+                margin-bottom:15px;
+            }
+        </style>
+        
+    </head>
+    
+    <body data-spy="scroll" data-target=".navbar-collapse">
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>                     
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                     </button>
+                     <a href="" class="navbar-brand">Town of Salem Companion</a>
+                </div>
+                
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="rankedpractice.html">Ranked Practice</a></li>
+                        <li class="active"><a href="contact.php">Contact Us</a></li>
+                        <li><a href="donate.html">Donate</a></li>
+                        <li><a href="privacypolicy.html">Privacy Policy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+   
+        <div class="container">
+            <div class="row">
+                <div class="center col-md-12 emailf">
+                    <h1>Our Contact Form</h1>
+                    
+                    <?php
+                        echo $result;
+                    ?>
+                    
+                    </p class="lead">Send me anything and then we can get in touch.</p>
+                    <form method="post">
+                        <div class="form-group">
+                            <label for="name">Your Name:</label>
+                            <input value="<?php echo $_POST["name"]; ?>" placeholder="Your Name"type="text" name="name" class="form-control">
+                            </input>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Your Email:</label>
+                            <input value="<?php echo $_POST["email"]; ?>" placeholder="Your Email"type="email" name="email" class="form-control">
+                            </input>
+                        </div>
+                        <div class="form-group">
+                            <label for="comment">Your Comment:</label>
+                            <textarea placeholder="Your Comment"name="comment" class="form-control"><?php echo $_POST["comment"]; ?></textarea>
+                        </div>
+                        <input type="submit" name="submit" class="subm btn btn-success" value="Submit"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
